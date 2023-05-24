a570086fb3e475a0fbdd2cd382fe004b59421bdb
IAN FRANCISCO ROLAND DO NASCIMENTO <0040482221028@americana.fatec.br>
2023-05-24 09:33:20 -0300

    Star5t

---
+++ projeto_2/ADS_2023/RecoverExceptions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ADS_2023
+{
+
+    /// <summary>
+    /// Responsável por salvar as exceções em um arquivo de texto
+    /// </summary>
+    public static class RecoverExceptions
+    {
+        /// <summary>
+        /// Retorna o caminho físico do arquivo de "LogExceptions.txt"
+        /// </summary>
+        public static string GetCaminhoFisico()
+        {
+            //Retorna o caminho físico do arquivo de "LogExceptions.txt"
+            string caminhoFisico = System.Web.HttpContext.Current.Server.MapPath("~/LogExceptions.txt");
+
+            return caminhoFisico;
+        }
+
+        /// <summary>
+        /// Salva as exceções em um arquivo de texto
+        /// </summary>
+        public static void SaveExceptions_(Exception ex)
+        {
+            string conteudo = "Date: " + DateTime.Now.ToString() + "\n";
+            conteudo += "Message: " + ex.Message + "\n";
+            conteudo += "Type: " + ex.GetType().ToString() + "\n";
+            conteudo += "Source: " + ex.Source + "\n";
+            conteudo += "StackTrace: " + ex.StackTrace + "\n";
+            conteudo += "TargetSite: " + ex.TargetSite + "\n";
+            conteudo += "--------------------------------------------------\n";
+
+            System.IO.File.AppendAllText(GetCaminhoFisico(), conteudo);
+
+        }
+
+        /// <summary>
+        /// Salva as exceções em um arquivo de texto
+        /// </summary>
+        public static void SaveExceptions(Exception ex)
+        {
+            string filePath = GetCaminhoFisico();
+            List<Dictionary<string, string>> exceptions = new List<Dictionary<string, string>>();
+
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+                if (!string.IsNullOrEmpty(jsonContent))
+                {
+                    try
+                    {
+                        exceptions = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(jsonContent);
+                    }
+                    catch (JsonException)
+                    {
+                        // O arquivo pode estar corrompido ou em um formato inválido. 
+                        // Neste caso, ignore o erro e comece com uma lista vazia de exceções.
+                    }
+                }
+            }
+
+            Dictionary<string, string> exceptionData = new Dictionary<string, string>
+            {
+                { "Date", DateTime.Now.ToString() },
+                { "Message", ex.Message },
+                { "Type", ex.GetType().ToString() },
+                { "Source", ex.Source },
+                { "StackTrace", ex.StackTrace },
+                { "TargetSite", ex.TargetSite?.ToString() }
+            };
+
+            exceptions.Add(exceptionData);
+
+            string jsonExceptions = JsonConvert.SerializeObject(exceptions, Formatting.Indented);
+            File.WriteAllText(filePath, jsonExceptions);
+        }
+
+        /// <summary>
+        /// Lê as exceções salvas no arquivo de texto
+        /// </summary>
+        public static string ReadExceptions_()
+        {
+            string conteudo = System.IO.File.ReadAllText(GetCaminhoFisico());
+            conteudo = conteudo.Replace("\n", "<br />");
+            return conteudo;
+        }
+
+        /// <summary>
+        /// Lê as exceções salvas no arquivo de texto
+        /// </summary>
+        public static List<Dictionary<string, string>> ReadExceptions()
+        {
+            string filePath = GetCaminhoFisico();
+            List<Dictionary<string, string>> exceptions = new List<Dictionary<string, string>>();
+
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+                if (!string.IsNullOrEmpty(jsonContent))
+                {
+                    exceptions = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(jsonContent);
+                }
+            }
+
+            return exceptions;
+        }
+    }
+
+}
---
+++ projeto_2/site/Admin/CadastroUsuarios.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+using System.Web;
+using Datapost.Access;
+
+namespace site.Admin
+{
+    public partial class CadastroUsuarios : System.Web.UI.Page
+    {
+        // https://www.connectionstrings.com/access
+        string conexao = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={HttpContext.Current.Server.MapPath("~/App_Data/Usuarios.accdb")};Persist Security Info=False;";
+
+        DAO acesso = new DAO();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CarregarGrid();
+            }
+        }
+
+        protected void btnInserir_Click(object sender, EventArgs e)
+        {
+            // Validar os campos do formulário
+            if (txtNomeAcesso.Text == string.Empty)
+            {
+                lblMensagem.Text = "O campo Nome é obrigatório!";
+                return;
+            } else {
+                string comando = "";
+
+                if (Codigo.Text == "")
+                {
+                    comando = $"INSERT INTO Usuarios (NomeCompleto, Email, NomeAcesso, Anotacoes, Senha) VALUES ('{txtNomeCompleto.Text}', '{txtEmail.Text}', '{txtNomeAcesso.Text}', '{txtAnotacoes.Text}', '{txtSenha.Text}')";
+                }
+                else
+                {
+                    comando = $"UPDATE Usuarios SET NomeCompleto='{txtNomeCompleto.Text}', Email='{txtEmail.Text}', NomeAcesso='{txtNomeAcesso.Text}', Anotacoes='{txtAnotacoes.Text}', Senha='{txtSenha.Text}' WHERE Codigo = {Codigo.Text}";
+
+                }
+                acesso.DataProviderName = DAO.ProviderName.OleDb;
+                acesso.ConnectionString = conexao;
+                acesso.Query(comando);
+
+                CarregarGrid();
+                LimparCampos();
+            }
+            lblMensagem.Text = "Usuário cadastrado com sucesso!";
+        }
+
+        protected void LimparCampos()
+        {
+            Codigo.Text = "";
+            txtNomeCompleto.Text = "";
+            txtEmail.Text = "";
+            txtNomeAcesso.Text = "";
+            txtAnotacoes.Text = "";
+            txtSenha.Text = "";
+            btnSalvar.Text = "Inserir";
+            btnExcluir.Visible = false;
+        }
+
+        protected void CarregarGrid()
+        {
+            //Carrega o GridView com os dados da tabela Usuarios
+            string comando = "SELECT Codigo, NomeCompleto FROM Usuarios ORDER BY NomeCompleto;";
+            acesso.DataProviderName = DAO.ProviderName.OleDb;
+            acesso.ConnectionString = conexao;
+            ViewUsuarios.DataSource = acesso.Query(comando);
+            ViewUsuarios.DataBind();
+        }
+
+        protected void ViewUsuarios_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            //Preenche os campos do formulário com os dados do registro selecionado no GridView
+            Codigo.Text = ViewUsuarios.SelectedRow.Cells[1].Text;
+            string comando = $"SELECT * FROM Usuarios WHERE Codigo = {Codigo.Text};";
+
+            acesso.DataProviderName = DAO.ProviderName.OleDb;
+            acesso.ConnectionString = conexao;
+            DataTable tabela = new DataTable();
+            tabela = (DataTable)acesso.Query(comando);
+
+            txtNomeCompleto.Text = tabela.Rows[0]["NomeCompleto"].ToString();
+            txtEmail.Text = tabela.Rows[0]["Email"].ToString();
+            txtNomeAcesso.Text = tabela.Rows[0]["NomeAcesso"].ToString();
+            txtAnotacoes.Text = tabela.Rows[0]["Anotacoes"].ToString();
+            txtSenha.Text = tabela.Rows[0]["Senha"].ToString();
+
+            btnSalvar.Text = "Alterar";
+            btnExcluir.Visible = true;
+        }
+
+        protected void btnExcluir_Click(object sender, EventArgs e)
+        {
+            string comando = $"DELETE FROM Usuarios WHERE Codigo = {Codigo.Text};";
+
+            acesso.DataProviderName = DAO.ProviderName.OleDb;
+            acesso.ConnectionString = conexao;
+            acesso.Query(comando);
+            CarregarGrid();
+            LimparCampos();
+            lblMensagem.Text = "Usuário excluído com sucesso!";
+        }
+
+        protected void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+    }
+}
